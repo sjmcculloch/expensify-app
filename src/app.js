e79a932b3dbbca846afbfc595af37bc305d4c18b
Scott McCulloch <smcculloch@gmail.com>
2018-02-11 14:56:01 +1100

    remove summy expense data

--- src/app.js
+++ src/app.js
@@ -12,15 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-// addExpense -> Water bill
-
-store.dispatch(addExpense({description: 'Water Bill', amount: 4500}));
-store.dispatch(addExpense({description: 'Gas Bill', amount: 1000, createdAt: 2000}));
-store.dispatch(addExpense({description: 'Rent', amount: 109500}));
-//store.dispatch(setTextFilter('bill'));
-
-const state = store.getState();
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
